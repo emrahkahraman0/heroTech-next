7c324a87e3438e29a2c77f3549c6afc3c402403a
emrahkahraman0 <oempire34@gmail.com>
2023-09-30 23:57:33 +0300

    update idea

--- app/page.js
+++ app/page.js
@@ -3,7 +3,18 @@ import React from "react";
 function Home() {
   return (
     <>
-      <div>Home</div>
+      <div
+        style={{
+          height: "500px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Home Page</h1>
+        <p>Apilere ulaşmak için menüdeki linklere gidebilirsiniz :)</p>
+      </div>
     </>
   );
 }
